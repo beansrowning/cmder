59039ed0097cc9b3d7b44df94a52048070d4d48e
Sean Browning <oet5@cdc.gov>
2017-11-21 11:09:04 -0500

    Fixed lambda blending into the background of the terminal. Updated some settings.

--- vendor/clink.lua
+++ vendor/clink.lua
@@ -38,7 +38,7 @@ function set_prompt_filter()
     -- build our own prompt λ
     -- orig: $E[1;32;40m$P$S{git}{hg}$S$_$E[1;30;40m{lamb}$S$E[0m
     -- color codes: "\x1b[1;37;40m"
-    local cmder_prompt = "\x1b[1;32;40m{cwd} {git}{hg}{svn} \n\x1b[1;30;40m{lamb} \x1b[0m"
+    local cmder_prompt = "\x1b[1;32;40m{cwd} {git}{hg}{svn} \n\x1b[1;39;40m{lamb} \x1b[0m"
     cmder_prompt = string.gsub(cmder_prompt, "{cwd}", cwd)
     if env == nil then
         lambda = "$"
